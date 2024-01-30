fad1638d0a8b01bad478ebddc5ae5f9d405d66f9
Icaro Félix <ifeme08@gmail.com>
2024-01-30 16:57:00 -0300

    Initial commit

---
+++ src/app/components/currency-card/currency-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './currency-card.component.html',
+  styleUrl: './currency-card.component.scss'
+})
+export class CurrencyCardComponent {
+  
+}
